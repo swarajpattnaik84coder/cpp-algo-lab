5f86e98a37e7e37227fad318e53300e4a74bbda0
swarajpattnaik84coder <swarajpattnaik84@gmail.com>
2026-05-11 13:54:23 +0530

    fixed Next Permutation

--- arrays/NextPermutation.cpp
+++ arrays/NextPermutation.cpp
@@ -1,27 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void nextPermutation(vector<int>& nums) {
-     
-    
-    int n=nums.size();
-    int ind=-1;
-    for(int i=n-2;i>=0;i--){
-        if(nums[i+1]>nums[i]){
-            ind=i;
-            break;
-        }
-    }
-    if(ind==-1){
-        reverse(nums.begin(),nums.end());
-        return;
-    }
-    for(int i=n-1;i>ind;i--){       
-        if(nums[i]>nums[ind]){
-            swap(nums[i],nums[ind]);
-            break;
-        }
-    }
-    reverse(nums.begin() + ind + 1, nums.end());
 
-} 
+    void nextPermutation(vector<int>& nums) {
+        // Your code goes here
+        
+    int n=nums.size();
+    int i=n-2;
+    int ind=-1;
+for(int i=n-2;i>=0;i--){
+    if(nums[i]<nums[i+1]){
+        ind=i;
+        break;
+    }
+}
+if(ind==-1){
+    reverse(nums.begin(),nums.end());
+    return;
+}
+int j=n-1;
+while(j>ind){
+    if(nums[j]>nums[ind]) break;
+    j--;
+}
+swap(nums[ind],nums[j]);
+reverse(nums.begin()+ind+1,nums.end());
+return;
+    }
